93d8235b4aaeb2c435c65fda9ede187ecdbfedac
22 <60903333+nini22P@users.noreply.github.com>
2024-10-03 17:07:07 +0800

    feat: use noto sans sc font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from "next";
+import { Noto_Sans_SC } from 'next/font/google'
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "iM@S2 EDITOR",
   description: "iM@S2 EDITOR",
 };
+
+const notoSansSC = Noto_Sans_SC({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  display: 'swap',
+})
 
 export default function RootLayout({
   children,
@@ -14,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="zh-cn">
       <body
-        className={`antialiased`}
+        className={notoSansSC.className}
       >
         {children}
       </body>

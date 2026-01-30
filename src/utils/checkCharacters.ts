90f471e062de63c275ea31482df9556e3f748bf4
22 <60903333+nini22P@users.noreply.github.com>
2026-01-30 20:55:40 +0800

    bypass cjk unified ideographs on checkCharacters

--- src/utils/checkCharacters.ts
+++ src/utils/checkCharacters.ts
@@ -3,13 +3,19 @@ import characters from './characters'
 const checkCharacters = (inputString: string): string[] => {
   const validChars = new Set(characters)
   const invalidChars: string[] = []
-  for (const char of inputString) {
-    const charCodeHex = char.charCodeAt(0).toString(16).toUpperCase().padStart(4, '0')
 
+  const CJKUnifiedIdeographRegex = /\p{Unified_Ideograph}/u
+
+  for (const char of inputString) {
     if (validChars.has(char) || ['\n', ' '].includes(char)) {
       continue
     }
 
+    if (CJKUnifiedIdeographRegex.test(char)) {
+      continue
+    }
+
+    const charCodeHex = char.charCodeAt(0).toString(16).toUpperCase().padStart(4, '0')
     if (charCodeHex.match(/^E[0-9A-Fa-f]{3}$/)) {
       continue
     }
